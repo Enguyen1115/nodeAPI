8c23ae8a9c294c2264a0f3227e7b30cfef427d0f
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-07-20 12:21:05 -0600

    Modify api.js & create student.js

--- router/api.js
+++ router/api.js
@@ -1,28 +1,64 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+// router.get('/students', function (req, res) {
+//     res.send({type: 'GET'});
+// });
+
+
+// router.post('/students', function (req, res) {
+//     res.send({
+//         type: 'POST',
+//         name: req.body.name,
+//         role: req.body.role,
+//     });
+// });
+
+
+// router.put('/students/:id', function (req, res) {
+//     res.send({type: 'PUT'});
+// });
+
+
+// router.delete('/students/:id', function (req, res) {
+//     res.send({type: 'DELETE'});
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
+const Student = require('../models/student')
 
-
-router.get('/students', function (req, res) {
-    res.send({type: 'GET'});
+//Get a list of students from the db
+router.get('/students', function (req, res, next) {
+    Student.find({}).then(function (students) {
+        res.send(students);
+    }).catch(next);
 });
 
+//Add a new student to db
+router.post('/students', function (req, res, next) {
+    Student.create(req.body).then(function (student) {
+        res.send(student);
+    }).catch(next);
+});
 
-router.post('/students', function (req, res) {
-    res.send({
-        type: 'POST',
-        name: req.body.name,
-        role: req.body.role,
+//Update student information in db
+router.put('/students/:id', function (req, res, next) {
+    Student.findOneAndUpdate({ id: req.params.id }, req.body).then(function (student) {
+        Student.findOne({ id: req.params.id }).then(function (student) {
+            res.send(student);
+        })
     });
 });
 
-
-router.put('/students/:id', function (req, res) {
-    res.send({type: 'PUT'});
-});
-
-
-router.delete('/students/:id', function (req, res) {
-    res.send({type: 'DELETE'});
+//Delete a student from db
+router.delete('/students/:id', function(req, res, next){
+    Student.findOneAndDelete({id: req.params.id}).then(function(student){
+        res.send(student);
+    });
 });
 
 module.exports = router;
